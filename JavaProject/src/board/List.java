a8d3653e11bca407e285d1c3f95f10885c56c5c1
SIST164 <SIST164@DESKTOP-1FR7EHV>
2020-02-24 10:36:24 +0900

    목폭 페이지 추가

---
+++ JavaProject/src/board/List.java
@@ -0,0 +1,9 @@
+package board;
+
+public class List {
+
+	public static void main(String[] args) {
+		System.out.println("쓰기 페이지 구현 중...");
+	}
+
+}
---
+++ JavaProject/src/board/Write.java
@@ -0,0 +1,9 @@
+package board;
+
+public class Write {
+
+	public static void main(String[] args) {
+		System.out.println("게시판 목록 페이지 구현 중...");
+	}
+
+}
